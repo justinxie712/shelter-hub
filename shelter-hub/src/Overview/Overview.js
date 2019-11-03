94bea6f560432e16e2a2d9937c02b43abc9ae900
Vanessa <vtostado@wesleyan.edu>
2019-11-03 09:57:56 -0800

    dynamic search

--- shelter-hub/src/Overview/Overview.js
+++ shelter-hub/src/Overview/Overview.js
@@ -2,25 +2,54 @@ import React, { Component } from 'react';
 //import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import './Overview.css';
+import { Button, Form, FormGroup, Label, Input,Container, Row, Col  } from 'reactstrap';
+import firebase from '../firebase.js';
 
 export class Overview extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
+              {latitude: 47.359423, longitude: -122.021071},
+              {latitude: 47.2052192687988, longitude: -121.988426208496},
+              {latitude: 47.6307081, longitude: -122.1434325},
+              {latitude: 47.3084488, longitude: -122.2140121},
+              {latitude: 47.5524695, longitude: -122.0425407}]
+    }
+  }
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return <Marker key={index} id={index} position={{
+       lat: store.latitude,
+       lng: store.longitude
+     }}
+     onClick={() => console.log("You clicked me!")} />
+    })
+  }
+
     render() {
         return (
-            <React.Fragment>
-                <Map
-                    google={this.props.google}
-                    zoom={17}
-                    style={{
-                        width: '90%',
-                        height: '100%',
-                        position: 'relative',
-                        float: 'right'
-                    }}
-                    initialCenter={{ lat: 47.444, lng: -122.176}}
-                >
-                    <Marker position={{ lat: 47.444, lng: -122.176}} />
-                </Map>
-            </React.Fragment>
+        <div>
+          <h1> Find Beds Near You </h1>
+          <Container id ="mapArea">
+            <Row>
+               <Col id = "mapView" sm="12" md={{ size: 6, offset: 3 }}>
+                <React.Fragment>
+                   <Map
+                       google={this.props.google}
+                       zoom={13}
+                       initialCenter={{ lat:37.77493, lng:-122.41942}}
+                   >
+                       {this.displayMarkers()}
+                   </Map>
+               </React.Fragment>
+
+             </Col>
+          </Row>
+        </Container>
+      </div>
+
         );
     }
 }
--- shelter-hub/src/Registration/Registration.js
+++ shelter-hub/src/Registration/Registration.js
@@ -1,95 +1,142 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import {
-  Link
-} from "react-router-dom";
+
+import React, { Component } from 'react';
+//import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+import { Button, Form, FormGroup, Label, Input,Container, Row, Col  } from 'reactstrap';
 import firebase from '../firebase.js';
+class ParlorForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = this.initialState()
+    this.handlePlaceSelect = this.handlePlaceSelect.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.autocomplete = null
+  }
 
-
-class Register extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      organization: '',
-      beds: '',
-      items: []
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-      organization: this.state.organization,
-      beds: this.state.beds
-    }
-    itemsRef.push(item);
-    this.setState({
-      organization: '',
-      beds: ''
-    });
-  }
   componentDidMount() {
+    this.autocomplete = new window.google.maps.places.Autocomplete(document.getElementById('autocomplete'), {})
+
+    this.autocomplete.addListener("place_changed", this.handlePlaceSelect)
     const itemsRef = firebase.database().ref('items');
     itemsRef.on('value', (snapshot) => {
       let items = snapshot.val();
       let newState = [];
       for (let item in items) {
         newState.push({
-          organization: items[item].organization,
-          beds: items[item].beds
+          name: items[item].name,
+          beds: items[item].beds,
+          street_address:items[item].street_address,
+          city: items[item].city,
+          state: items[item].state,
+          zip_code:items[item].zipcode
         });
       }
       this.setState({
         items: newState
       });
     });
-  }
- render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-          <input type="TEXT" name="organization" id="orgName" placeholder="Your organization's name" onChange={this.handleChange} value={this.state.organization} />
-          <Label for="orgEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Your organization's email" />
-          <Label for="Password">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="" />
-          <Label for="Street">Street</Label>
-          <Input type="text" name="street" id="street" placeholder="" />
-          <Label for="City">City</Label>
-          <Input type="text" name="city" id="city" placeholder="" />
-          <Label for="State">State</Label>
-          <Input type="text" name="state" id="state" placeholder="" />
-          <Label for="ZIP">ZIP</Label>
-          <Input type="text" name="zip" id="zip" placeholder="" />
-          <Label for="Country">Country</Label>
-          <Input type="text" name="country" id="country" placeholder="" />
-          <Label for="operatingHours">Hours of Operation</Label>
-          <br/>From: <Input type="time" name="startTime" id="startTime" />
-          To: <Input type="time" name="startTime" id="startTime" />
-          <Label for="operatingDays">Which days are you open?</Label>
-          <Input type="select" name="selectDays" id="selectDays" multiple>
-            <option>Mon</option>
-            <option>Tues</option>
-            <option>Wed</option>
-            <option>Thurs</option>
-            <option>Fri</option>
-            <option>Sat</option>
-            <option>Sun</option>
-          </Input>
 
-         <Label for="numberOfBeds">How many beds do you have available?</Label>
-         <input type="number" name="beds" id="numberBeds" onChange={this.handleChange} value={this.state.beds}/>
-        <Link to="/">Go Back </Link>
-        <Button>Submit</Button>
-      </Form>
-    );
   }
+
+  initialState() {
+    return {
+      name: '',
+      beds: '',
+      street_address: '',
+      city: '',
+      state: '',
+      zip_code: '',
+    }
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit(event) {
+  event.preventDefault()
+  const itemsRef = firebase.database().ref('items');
+  const item = {
+   name: this.state.name,
+   beds: this.state.beds,
+   street_address: this.state.street_address,
+   city: this.state.city,
+   state: this.state.state,
+   zip_code: this.state.zipcode
+
+ }
+ itemsRef.push(item);
+ this.setState({
+   name: '',
+   beds: '',
+   street_address: '',
+   city: '',
+   state: '',
+   zip_code: '',
+ });
 }
 
-export default Register;
+  handlePlaceSelect() {
+    let addressObject = this.autocomplete.getPlace()
+    let address = addressObject.address_components
+    debugger
+    this.setState({
+      name: addressObject.name,
+      beds: addressObject.beds,
+      street_address: `${address[0].long_name} ${address[1].long_name}`,
+      city: address[2].long_name,
+      state: address[4].short_name,
+      zip_code: address[6].short_name,
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Add New Parlor</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input id="autocomplete"
+            className="input-field"
+            ref="input"
+            type="text"/>
+            <input
+              name={"name"}
+              value={this.state.name}
+              placeholder={"Name"}
+              onChange={this.handleChange}
+            />
+            <input
+              name={"street_address"}
+              value={this.state.street_address}
+              placeholder={"Street Address"}
+              onChange={this.handleChange}
+            />
+            <input
+              name={"city"}
+              value={this.state.city}
+              placeholder={"City"}
+              onChange={this.handleChange}
+            />
+            <input
+              name={"state"}
+              value={this.state.state}
+              placeholder={"State"}
+              onChange={this.handleChange}
+            />
+            <input
+              name={"zip_code"}
+              value={this.state.zip_code}
+              placeholder={"Zipcode"}
+              onChange={this.handleChange}
+            />
+            <button onSubmit={this.handleSubmit}>Submit</button>
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default ParlorForm
