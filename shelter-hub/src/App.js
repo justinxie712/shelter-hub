749dab1c07ccef6b3d952c61c17c1564a3e5640e
Justin Xie <juxie@paloaltonetworks.com>
2019-11-02 20:19:24 -0700

    map overview view

--- shelter-hub/src/App.js
+++ shelter-hub/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login/Login';
+import Overview from './Overview/Overview'
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <h1>ShelterHub</h1>
-      <Login />
+      {/* <Login /> */}
+      <Overview />
     </div>
   );
 }
