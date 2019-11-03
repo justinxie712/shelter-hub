2fc088e7c4caa99ca7c0f88ea7e55a0a2b5abd8f
Justin Xie <juxie@paloaltonetworks.com>
2019-11-03 10:40:38 -0800

    added tooltip to markers

--- shelter-hub/src/Overview/Overview.js
+++ shelter-hub/src/Overview/Overview.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-//import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import './Overview.css';
-import { Button, Form, FormGroup, Label, Input,Container, Row, Col  } from 'reactstrap';
+import { Container, Row, Col  } from 'reactstrap';
 import firebase from '../firebase.js';
 
 export class Overview extends Component {
@@ -10,48 +9,72 @@ export class Overview extends Component {
     super(props);
 
     this.state = {
-      stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
-              {latitude: 47.359423, longitude: -122.021071},
-              {latitude: 47.2052192687988, longitude: -121.988426208496},
-              {latitude: 47.6307081, longitude: -122.1434325},
-              {latitude: 47.3084488, longitude: -122.2140121},
-              {latitude: 47.5524695, longitude: -122.0425407}]
+      stores: [{latitude: 47.49855629475769, longitude: -122.14184416996333},
+        {latitude: 47.359423, longitude: -122.021071},
+        {latitude: 47.2052192687988, longitude: -121.988426208496},
+        {latitude: 47.6307081, longitude: -122.1434325},
+        {latitude: 47.3084488, longitude: -122.2140121},
+        {latitude: 47.5524695, longitude: -122.0425407}
+      ],
+      activeMarker: {},
+      selectedPlaces: {},
+      showInfoWindow: true,
     }
   }
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
-    })
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showInfoWindow: true
+    });
   }
 
-    render() {
-        return (
-        <div>
+
+  displayMarkers = (marker, index) => {
+    return (
+        <Marker
+          key={index} 
+          id={index} 
+          position={{
+          lat: marker.latitude,
+          lng: marker.longitude
+        }}
+        onClick={this.onMarkerClick} />
+    )
+  }
+
+  render() {
+    return (
+      <div>
           <h1> Find Beds Near You </h1>
           <Container id ="mapArea">
             <Row>
-               <Col id = "mapView" sm="12" md={{ size: 6, offset: 3 }}>
+              <Col id = "mapView" sm="12" md={{ size: 6, offset: 3 }}>
                 <React.Fragment>
-                   <Map
-                       google={this.props.google}
-                       zoom={13}
-                       initialCenter={{ lat:37.77493, lng:-122.41942}}
-                   >
-                       {this.displayMarkers()}
-                   </Map>
-               </React.Fragment>
-
-             </Col>
+                  <Map
+                      google={this.props.google}
+                      zoom={13}
+                      initialCenter={{ lat:47.49855629475769, lng:-122.14184416996333}}
+                  >
+                    {this.state.stores.map((marker, index) => {
+                      return this.displayMarkers(marker, index);
+                    })}
+                    <InfoWindow
+                      marker={this.state.activeMarker}
+                      visible={this.state.showInfoWindow}>
+                        <div>
+                          <h1>{`${this.state.activeMarker.id} beds`}</h1>
+                        </div>
+                    </InfoWindow>
+                  </Map>
+              </React.Fragment>
+            </Col>
           </Row>
         </Container>
       </div>
-
-        );
-    }
+    );
+  }
 }
 
 export default GoogleApiWrapper({
