06708b2cdccbbecc3c70283db9610f255cd7cb31
Vanessa <vtostado@wesleyan.edu>
2019-11-03 00:56:45 -0700

    linking pages

--- shelter-hub/src/App.js
+++ shelter-hub/src/App.js
@@ -5,6 +5,12 @@ import Login from './Login/Login';
 import Overview from './Overview/Overview';
 import SideNav from './SideNav/SideNav';
 import Registration from './Registration/Registration';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -13,9 +19,13 @@ function App() {
     <React.Fragment>
 
       <div className="App">
-        <Login />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact={true} component={Login}></Route>
+            <Route path="/Registration/Registration" exact={true} component={Registration}></Route>
+          </Switch>
+        </BrowserRouter>
         {/* <Overview /> */}
-        <Registration />
       </div>
     </React.Fragment>
   );
--- shelter-hub/src/Login/Login.js
+++ shelter-hub/src/Login/Login.js
@@ -5,6 +5,10 @@ import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input,Container, Row, Col  } from 'reactstrap';
 import './Login.css';
 import firebase, { auth, provider } from '../firebase.js';
+import {
+  Link
+} from "react-router-dom";
+
 
 
 
@@ -28,13 +32,12 @@ class Login extends React.Component {
              <h1> Sign in to Velocity </h1>
              <p> Please enter your credentails to proceed </p>
              <Button onClick={this.login}>Log In</Button>
-             <h3>Don't have an account? <a href =""> Sign up</a></h3>
-
            </Col>
            <Col>
-              <img src="/resources/main.jpg"></img>
+              <img src="./resources/main.jpg"></img>
            </Col>
          </Row>
+         <h3>Don't have an account? <Link to="/Registration/Registration"> Sign up </Link></h3>
        </Container>
      )
 
