989bb9eab618d2580fce975161ba0637aa9228a2
Justin Xie <juxie@paloaltonetworks.com>
2019-11-03 00:19:12 -0700

    fefe

--- shelter-hub/src/App.js
+++ shelter-hub/src/App.js
@@ -7,17 +7,17 @@ import SideNav from './SideNav/SideNav';
 import Registration from './Registration/Registration';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const [isLoggedIn, setIsLoggedIn] = useState(true); 
 
   return (
     <React.Fragment>
       {isLoggedIn && <SideNav />}
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>ShelterHub</h1>
-        {/*<Login />*/}
-        {/* <Overview /> */}
-        <Registration />
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <h1>ShelterHub</h1> */}
+        {/* <Login /> */}
+        <Overview />
+        {/* <Registration /> */}
       </div>
     </React.Fragment>
   );
